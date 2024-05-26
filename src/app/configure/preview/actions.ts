4f07bdaed99e2d3a667cc86c82f880a082eb4396
Dipesh <dtamangt582@gmail.com>
2024-05-26 14:07:44 +0530

    Added new feature to preview the design and checkout with stripe

---
+++ src/app/configure/preview/actions.ts
@@ -0,0 +1,87 @@
+"use server";
+
+import { auth } from "@/auth";
+import { BASE_PRICE, PRODUCT_PRICES } from "@/config/products";
+import { getUserByEmail } from "@/data/user";
+import { db } from "@/db";
+import { stripe } from "@/lib/stripe";
+import { Order } from "@prisma/client";
+
+export const createCheckoutSession = async ({
+  configId,
+}: {
+  configId: string;
+}) => {
+  const configuration = await db.configuration.findUnique({
+    where: { id: configId },
+  });
+
+  if (!configuration) {
+    throw new Error("No such configuration found");
+  }
+
+  const session = await auth();
+  const user = session?.user;
+
+  if (!user || !user.email) {
+    throw new Error("You need to be logged in");
+  }
+
+  const existingUser = await getUserByEmail(user.email);
+
+  if (!existingUser) {
+    throw new Error("User not found");
+  }
+
+  const { finish, material } = configuration;
+
+  let price = BASE_PRICE;
+  if (finish === "textured") price += PRODUCT_PRICES.finish.textured;
+  if (material === "polycarbonate")
+    price += PRODUCT_PRICES.material.polycarbonate;
+
+  let order: Order | undefined = undefined;
+
+  const existingOrder = await db.order.findFirst({
+    where: {
+      userId: existingUser.id,
+      configurationId: configuration.id,
+    },
+  });
+
+  if (existingOrder) {
+    order = existingOrder;
+  } else {
+    order = await db.order.create({
+      data: {
+        amount: price,
+        userId: existingUser.id,
+        configurationId: configuration.id,
+      },
+    });
+  }
+
+  const product = await stripe.products.create({
+    name: "Custom iPhone Case",
+    images: [configuration.imageUrl],
+    default_price_data: {
+      currency: "INR",
+      unit_amount: price,
+    },
+  });
+
+  const stripeSession = await stripe.checkout.sessions.create({
+    success_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/thank-you?orderId={order.id}`,
+    cancel_url: `${process.env.NEXT_PUBLIC_SERVER_URL}/configure/preview?id=${configuration.id}`,
+    payment_method_types: ["card"],
+    mode: "payment",
+    shipping_address_collection: { allowed_countries: ["IN"] },
+    metadata: {
+      userId: existingUser.id,
+      orderId: order.id,
+    },
+    line_items: [{ price: product.default_price as string, quantity: 1 }],
+  });
+
+  return { url: stripeSession.url };
+};
